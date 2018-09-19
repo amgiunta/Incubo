e46abc41b1544e2b522b1ce7bcf1425146d13d48
amgiunta.2016 <amgiunta.2016@mymail.becker.edu>
2018-09-18 21:50:55 -0400

    [Level Loading] Added a rudimentary system for swapping levels when an object with a specific tag triggers a collider
    The LoadTrigger.cs script utilizes unity's trigger functionality to load a desired Scene by name. Input the name of the desired Scene in the levelName field, and include the tag of the object in the tagsMask list to load a level on trigger.
    
    Signed-off-by: amgiunta.2016 <amgiunta.2016@mymail.becker.edu>

---
+++ Incubo/Assets/Scripts/LoadTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTrigger : MonoBehaviour {
+
+    [Tooltip("The tags of objects that can activate this trigger when entering it.")]
+    /// <summary>
+    /// The tags of objects that can activate this trigger when entering it.
+    /// </summary>
+    public List<string> tagMask;
+    [Tooltip("The name of the level you want to load.")]
+    /// <summary>
+    /// The name of the level you want to load.
+    /// </summary>
+    public string levelName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the object that entered the trigger has a tag in the list of valid tags...
+        if (tagMask.Contains(collision.gameObject.tag))
+        {
+            // Try to load the desired scene from the stored build index.
+            SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+        }
+    }
+}
