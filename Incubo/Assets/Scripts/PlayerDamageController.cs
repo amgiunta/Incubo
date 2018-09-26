2503a0dd8b025ed55f6f523c156494db6e7b5f3e
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2018-09-26 12:29:46 -0400

    Commit for Merge
    Signed-off-by: Adam Giunta <amgiunta.2016@mymail.becker.edu>

--- Incubo/Assets/Scripts/PlayerDamageController.cs
+++ Incubo/Assets/Scripts/PlayerDamageController.cs
@@ -10,6 +10,7 @@ public class PlayerDamageController : Character {
     //public Text DeathText;
 
     public float fearTickTime = 1f;
+    public int fearRestoreTick = 1;
 
     //float currentHealth;
     bool inFearZone;
@@ -20,6 +21,15 @@ public class PlayerDamageController : Character {
         inFearZone = false;
         InvokeRepeating("FearTicker", 0f, fearTickTime);
 	}
+
+    //Temp Code to test Enemy Death
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -36,6 +46,15 @@ public class PlayerDamageController : Character {
             //Note: Change to variable when avalable
             TakeDamage(1);
             inFearZone = false;
+        }
+        else if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0 && currentFear > 0)
+        {
+            if (currentFear - fearRestoreTick < 0)
+            {
+                int excessRestore = -(currentFear - fearRestoreTick);
+                TakeDamage(-(fearRestoreTick - excessRestore));
+            }
+            else { TakeDamage(-fearRestoreTick); }
         }
     }
 
